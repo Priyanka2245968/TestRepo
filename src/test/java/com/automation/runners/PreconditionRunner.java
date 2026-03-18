b4ebb5108d9ef5589a2a6d0daee80733a81f145a
Priyanka2245968 <163841039+Priyanka2245968@users.noreply.github.com>
2026-03-18 21:51:16 +0530

    test: auto-generated scripts for 'Precondition - Page Object'
    Files:
    - `src/main/java/com/automation/pages/PreconditionPage.java`
    - `src/test/java/com/automation/runners/PreconditionRunner.java`
    - `src/test/java/com/automation/steps/PreconditionStepDefinitions.java`
    - `src/test/java/com/automation/tests/PreconditionTest.java`

---
+++ src/test/java/com/automation/runners/PreconditionRunner.java
@@ -0,0 +1,18 @@
+package com.automation.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"com.automation.steps"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/Cucumber.json",
+        "junit:target/cucumber-reports/Cucumber.xml"
+    },
+    monochrome = true
+)
+public class PreconditionRunner extends AbstractTestNGCucumberTests {
+}
