dc1312119d3598d50581bbd17ca5b6cf48827b64
Priyanka2245968 <163841039+Priyanka2245968@users.noreply.github.com>
2026-04-09 11:43:01 +0530

    test: auto-generated scripts for 'URL: https://www.w3schools.com - Page Object'
    Files:
    - `src/main/java/com/automation/pages/UrlHttpsWwwW3SchoolsComPage.java`
    - `src/test/java/com/automation/runners/UrlHttpsWwwW3SchoolsComRunner.java`
    - `src/test/java/com/automation/steps/UrlHttpsWwwW3SchoolsComSteps.java`
    - `src/test/java/com/automation/tests/UrlHttpsWwwW3SchoolsComTest.java`

---
+++ src/test/java/com/automation/steps/UrlHttpsWwwW3SchoolsComSteps.java
@@ -0,0 +1,38 @@
+package com.automation.steps;
+
+import cucumber.api.java.en.*;
+import org.openqa.selenium.WebDriver;
+
+public class UrlHttpsWwwW3SchoolsComSteps {
+    private WebDriver driver;
+    private UrlHttpsWwwW3SchoolsComPage page;
+
+    public UrlHttpsWwwW3SchoolsComSteps() {
+        // driver initialisation happens via hooks
+    }
+
+    @Given("^Navigate to https://www.w3schools.com$")
+    public void Navigate_to_https___www_w3schools_com() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Click on the Search input box at the top$")
+    public void Click_on_the_Search_input_box_at_the_top() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Click the Search button$")
+    public void Click_the_Search_button() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Enter the keyword "HTML"$")
+    public void Enter_the_keyword__HTML() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Press Enter$")
+    public void Press_Enter() throws Throwable {
+        // TODO: implement step
+    }
+}
