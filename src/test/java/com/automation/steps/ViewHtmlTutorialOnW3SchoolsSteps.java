2c3481dd8be9d951fe4212ba8a5bba8e6a131b2b
Priyanka2245968 <163841039+Priyanka2245968@users.noreply.github.com>
2026-04-24 17:18:54 +0530

    test: auto-generated scripts for 'View HTML Tutorial on W3Schools - Page Object'
    Files:
    - `src/main/java/com/automation/pages/ViewHtmlTutorialOnW3SchoolsPage.java`
    - `src/test/java/com/automation/runners/ViewHtmlTutorialOnW3SchoolsRunner.java`
    - `src/test/java/com/automation/steps/ViewHtmlTutorialOnW3SchoolsSteps.java`
    - `src/test/java/com/automation/tests/ViewHtmlTutorialOnW3SchoolsTest.java`

---
+++ src/test/java/com/automation/steps/ViewHtmlTutorialOnW3SchoolsSteps.java
@@ -0,0 +1,33 @@
+package com.automation.steps;
+
+import cucumber.api.java.en.*;
+import org.openqa.selenium.WebDriver;
+
+public class ViewHtmlTutorialOnW3SchoolsSteps {
+    private WebDriver driver;
+    private ViewHtmlTutorialOnW3SchoolsPage page;
+
+    public ViewHtmlTutorialOnW3SchoolsSteps() {
+        // driver initialisation happens via hooks
+    }
+
+    @Given("^Navigate to https://www.w3schools.com$")
+    public void Navigate_to_https___www_w3schools_com() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Click on the 'Tutorials' menu at the top$")
+    public void Click_on_the__Tutorials__menu_at_the_top() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Select 'HTML' from the list of topics$")
+    public void Select__HTML__from_the_list_of_topics() throws Throwable {
+        // TODO: implement step
+    }
+
+    @Given("^Scroll down and click on 'Learn HTML'$")
+    public void Scroll_down_and_click_on__Learn_HTML() throws Throwable {
+        // TODO: implement step
+    }
+}
---
+++ src/test/java/com/automation/tests/ViewHtmlTutorialOnW3SchoolsTest.java
@@ -0,0 +1,13 @@
+package com.automation.tests;
+
+import org.testng.annotations.Test;
+
+public class ViewHtmlTutorialOnW3SchoolsTest extends BaseTestManager {
+
+    @Test
+    public void testViewHtmlTutorialOnW3Schools() {
+        // End-to-end test: View HTML Tutorial on W3Schools
+        // URL: https://www.w3schools.com
+        System.out.println("Test: View HTML Tutorial on W3Schools");
+    }
+}
