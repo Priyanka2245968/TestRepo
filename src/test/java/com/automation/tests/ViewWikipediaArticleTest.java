382ecb4a79a0dd6785eda131e1019298b8486a5c
Priyanka2245968 <163841039+Priyanka2245968@users.noreply.github.com>
2026-04-24 17:25:02 +0530

    test: auto-generated scripts for 'View Wikipedia Article - Page Object'
    Files:
    - `src/main/java/com/automation/pages/ViewWikipediaArticlePage.java`
    - `src/test/java/com/automation/runners/ViewWikipediaArticleRunner.java`
    - `src/test/java/com/automation/steps/ViewWikipediaArticleSteps.java`
    - `src/test/java/com/automation/tests/ViewWikipediaArticleTest.java`

---
+++ src/test/java/com/automation/tests/ViewWikipediaArticleTest.java
@@ -0,0 +1,13 @@
+package com.automation.tests;
+
+import org.testng.annotations.Test;
+
+public class ViewWikipediaArticleTest extends BaseTestManager {
+
+    @Test
+    public void testViewWikipediaArticle() {
+        // End-to-end test: View Wikipedia Article
+        // URL: https://www.wikipedia.org/
+        System.out.println("Test: View Wikipedia Article");
+    }
+}
